a2159cb124c490fc72dae535ac23eed17e9b3f07
Dejorden <d.moerman@hotmail.nl>
2023-10-07 16:56:29 +0200

    remove standard components and add welcome

---
+++ resources/js/components/Welcomcomponent.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+<template>
+    <article class="bg-slate-100 md:flex rounded-xl p-8 dark:bg-slate-600 flex-col">
+        <h1 class="text-black dark:text-slate-50 font-medium text-3xl">Welkom</h1>
+        <p class="">Welkom at be acritic. The app to discuss with your friends about the latest movies and series.</p>
+    </article>
+</template>    
